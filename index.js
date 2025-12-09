93602467085fb3eed1a50c06821dca9ed24c7d37
eduardo-meik <eduardo@meiklabs.com>
2025-12-08 23:05:39 -0300

    Fix Express 5 route syntax error by removing redundant OPTIONS handler

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// Explicitly handle preflight requests
-app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 
